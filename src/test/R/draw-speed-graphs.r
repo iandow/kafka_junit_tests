b2a329a4ed2e806351ca4ab10dbde2ef2a8a06a7
Ian Downard <kayaking@gmail.com>
2016-10-16 21:07:40 -0700

    add message size test

--- src/test/R/draw-speed-graphs.r
+++ src/test/R/draw-speed-graphs.r
@@ -24,3 +24,13 @@ abline(v=18.5, col='lightgray')
 abline(v=24.5, col='lightgray')
 abline(v=30.5, col='lightgray')
 dev.off()
+
+
+
+png(file="size-count.png", width=800, height=500, pointsize=16)
+x = read.csv("size-count.csv")
+boxplot(batchRate/1e6 ~ topicCount + messageSize, x, ylim=c(0,1.0),
+        ylab="Millions of messages / second", xlab="Message Size (bytes)",
+        col=rainbow(6)[ceiling((1:36)/6)], xaxt='n')
+axis(1,labels=as.character(rep(c(100,500,1000,2000,10000,50000,100000),7)), at=(1:49), las=3)
+dev.off()

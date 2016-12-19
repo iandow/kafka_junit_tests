4c9d5780ff36643578ed6479bb797a2a2b765518
Ian Downard <kayaking@gmail.com>
2016-12-18 21:59:45 -0800

    misc update

--- src/test/R/draw-speed-graphs.r
+++ src/test/R/draw-speed-graphs.r
@@ -15,9 +15,9 @@ png(file="thread.png", width=800, height=500, pointsize=16)
 x = read.csv("thread-count.csv")
 boxplot(batchRate/1e6 ~ topicCount + threadCount, x, ylim=c(0,2.1), 
         ylab="Millions of messages / second", xlab="Topics",
-        col=rainbow(4)[ceiling((1:30)/6)], xaxt='n')
+        col=rainbow(6)[ceiling((1:36)/6)], xaxt='n')
 axis(1,labels=as.character(rep(c(50,100,200,500,1000,2000),6)), at=(1:36), las=3)
-legend(x=32,y=2.1,legend=c(1,2,5,10,15,20), col=rainbow(4), fill=rainbow(4), title="Threads")
+legend(x=32,y=2.1,legend=c(1,2,5,10,15,20), col=rainbow(6), fill=rainbow(6), title="Threads")
 abline(v=6.5, col='lightgray')
 abline(v=12.5, col='lightgray')
 abline(v=18.5, col='lightgray')
@@ -26,11 +26,21 @@ abline(v=30.5, col='lightgray')
 dev.off()
 
 
-
 png(file="size-count.png", width=800, height=500, pointsize=16)
 x = read.csv("size-count.csv")
 boxplot(batchRate/1e6 ~ topicCount + messageSize, x, ylim=c(0,1.0),
         ylab="Millions of messages / second", xlab="Message Size (bytes)",
         col=rainbow(6)[ceiling((1:36)/6)], xaxt='n')
 axis(1,labels=as.character(rep(c(100,500,1000,2000,10000,50000,100000),7)), at=(1:49), las=3)
+title("Message Size vs Throughput (Messages)")
 dev.off()
+
+png(file="size-count-bytes.png", width=800, height=500, pointsize=16)
+x = read.csv("size-count.csv")
+boxplot(batchRate*messageSize/1e6 ~ messageSize, x,
+        ylab="Throughput (MB/Second)", xlab="Message Size (Bytes)", ylim=c(0,200),
+        col=rainbow(6)[ceiling((1:36)/6)], xaxt='n')
+axis(1,labels=as.character(rep(c(100,500,1000,2000,10000,50000,100000),7)), at=(1:49), las=3)
+title("Message Size vs Throughput (MBs)")
+dev.off()
+
